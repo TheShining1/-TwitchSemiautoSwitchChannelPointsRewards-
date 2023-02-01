ecce0b6f70f31afea1588ea707dbf8715b5ed463
TheShiningOne <104907667+TheShining1@users.noreply.github.com>
2023-02-01 13:55:29 +0100

    Update SetActivePoints.cs

--- SetActivePoints.cs
+++ SetActivePoints.cs
@@ -6,16 +6,34 @@ using System.Threading.Tasks;
 
 public class CPHInline
 {
+	string LogPrefix = "SetActivePoints::";
+
 	public bool Execute()
 	{
-		List<string> requiredGroups = new List<string>{"Soundalerts"};
-		if (args.ContainsKey("targetGroupName")) requiredGroups.Add(args["targetGroupName"].ToString());
+		List<string> requiredGroups = new List<string>();
+		if (args.ContainsKey("genericGroupName") && args["genericGroupName"].ToString() != "")
+		{
+			CPH.LogDebug($"{LogPrefix}Adding generic groups: {args["genericGroupName"].ToString()}");
+			requiredGroups.AddRange(args["genericGroupName"].ToString().Split(','));
+		}
 
-		string filePath = @"data\settings.json";
+		if (args.ContainsKey("targetGroupName") && args.ContainsKey("targetGroupName").ToString() != "")
+		{
+			CPH.LogDebug($"{LogPrefix}Adding target group: {args["targetGroupName"].ToString()}");
+			requiredGroups.Add(args["targetGroupName"].ToString());
+		}
+
+		if (requiredGroups.Count == 0)
+		{
+			CPH.LogDebug($"{LogPrefix}No groups to enable");
+			return false;
+		}
+
+		CPH.LogDebug($"{LogPrefix}Getting rewards");
+		string filePath = @"data\twitch_rewards.json";
 		string jsonStr = File.ReadAllText(filePath);
 
-		JObject settingsJson = JObject.Parse(jsonStr);
-		JObject channelPointsJson = settingsJson.Value<JObject>("channelPoints");
+		JObject channelPointsJson = JObject.Parse(jsonStr);
 		JArray rewardsArray = channelPointsJson.Value<JArray>("rewards");
 
 		List<string> rewardList = new List<string>();
@@ -27,13 +45,14 @@ public class CPHInline
 			
 			if (requiredGroups.Exists(el => el.ToLower() == groupName.ToLower()))
 			{
-				CPH.LogInfo(groupName);
+				CPH.LogDebug($"{LogPrefix}Adding reward: {reward["name"].ToString()}");
 				rewardList.Add(rewardID);
 			}
 		}
 
 		Parallel.ForEach(rewardList, reward =>
 		{
+			CPH.LogDebug($"{LogPrefix}Enabling reward: {reward}");
 			CPH.EnableReward(reward);
 		});
 
